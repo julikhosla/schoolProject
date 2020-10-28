12fd867d0fb551748223278f1a4654394e7baed7
juliruth17 <julikhosla17@gmail.com>
2020-10-28 23:37:42 +0530

    crud

---
+++ src/app/services/tutorial.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+const baseUrl = 'http://localhost:8080/api/tutorials';
+@Injectable({
+  providedIn: 'root'
+})
+export class TutorialService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(){
+    return this.http.get(baseUrl);
+  }
+  get(id){
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+  create(data){
+    return this.http.post(baseUrl,data);
+  }
+  findByTitle(title){
+    return this.http.get(`${baseUrl}?title=${title}`)
+  }
+}
